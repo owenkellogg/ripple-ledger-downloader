b64d65fcf4ea55083ab64371bf30db835f3e8332
Steven Zeiler <zeiler.steven@gmail.com>
2014-07-03 13:38:36 -0700

    [FEATURE] Extract LedgerFormatter from test.

--- test/download_ledgers_recursively_going_back_in_time.js
+++ test/download_ledgers_recursively_going_back_in_time.js
@@ -2,30 +2,6 @@ var LedgerDownloader = require(__dirname+'/../lib/ledger_downloader.js');
 var _ = require('underscore-node');
 
 ledgerDownloader = new LedgerDownloader();
-
-function LedgerFormatter(ledger){
-  this.ledger = ledger;
-}
-
-LedgerFormatter.prototype = {
-  formatLedger: function() {
-    var self = this;
-    var output = {};  
-    output.ledger_index = self.ledger.ledger_index;
-    output.transactions = _.map(self.ledger.transactions, function(transaction) {
-      return {
-        hash: transaction.hash,
-        type: transaction.TransactionType
-      }
-    });
-    return output;
-  },
-  logFormattedLedger: function() {
-    var self = this;
-    console.log(self.formatLedger());
-  }
-}
-
 var currentLedgerIndex;
 
 ledgerDownloader.getMostRecentlyClosedLedger(function(error, ledger) {
