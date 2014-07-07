e08c1ae00ac760f3161209e80ceaa1b3365a5f1d
Steven Zeiler <zeiler.steven@gmail.com>
2014-07-06 21:18:14 -0700

    [FEATURE] Refactor looping downloader into LedgerDownloader.

--- test/download_ledgers_recursively_going_back_in_time.js
+++ test/download_ledgers_recursively_going_back_in_time.js
@@ -1,45 +1,26 @@
 var LedgerDownloader = require(__dirname+'/../lib/ledger_downloader.js');
-var LedgerFormatter = require(__dirname+'/../lib/ledger_formatter.js');
 var Ledger = require(__dirname+'/../lib/models/ledger.js');
 
-var ledgerDownloader = new LedgerDownloader();
-var currentLedgerIndex;
-
-ledgerDownloader.getMostRecentlyClosedLedger(function(error, ledger) {
-  console.log('got most recently closed ledger');
-  if (error) {
-    console.log('error', error);
-  } else {
-    currentLedgerIndex = ledger.ledger_index;
-    console.log('ledger', ledger.ledger_index);
-    getLedgersRecursively(getLedgersRecursively);
-  }
-});
-
-function getLedgersRecursively(callback) {
-  console.log('getLedgersRecursively');
-  ledgerDownloader.getLedger(currentLedgerIndex-1, function(error, ledger) {
-    if (error) {
-      console.log('error', error);
-      callback(callback);
-    } else {
-      var ledgerFormatter = new LedgerFormatter(ledger);
-      ledgerFormatter.logFormattedLedger();
-      currentLedgerIndex = ledger.ledger_index;
+var ledgerDownloader = new LedgerDownloader({
+  onLedger: function(ledger, next) {
+    Ledger.find({ where: {
+      id: ledger.ledger_index
+    }}).complete(function(error, record) {
+      if (error)  { return next(error, null); }
+      if (record) { return next(null, record.id); }
       Ledger.create({
         id: ledger.ledger_index,
         hash: ledger.hash    
-      }).complete(function(){
-        callback(callback);
+      }).complete(function(error, ledger){
+        if (error) {
+          next(error, null);
+        } else {
+          next(null, ledger.id);
+        }  
       });
-    }
-  })
-};
-/*
-downloader = new LedgerDownloader(function(ledger, next) {
-  Ledger.createFromRippledFormat(ledger).complete(next);
+    })
+  }
 });
 
-downloader.downloadLedgers();
+ledgerDownloader.start();
 
-*/
